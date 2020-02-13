f556bee6340191c0a3b258cfb95b8015744fa49c
JosueAntonio77 <60789948+JosueAntonio77@users.noreply.github.com>
2020-02-13 13:39:31 -0600

    Programa 3

---
+++ Josué Antonio Castro Collí/Programa 2.js
@@ -0,0 +1,21 @@
+/*
+****************************************
+* Source: Programa1.js 
+* Description: Funciones iniciales en js
+* Encender un motor / Apagar un motor 
+* Date: 7/Fec/2020
+* Author: Josue Antonio Castro Colli
+****************************************
+*/
+
+var calificacion1 = prompt("Escribe una calificación númerica");
+var calificacion2 = prompt("Escribe una calificación númerica");
+
+if (isNaN (calificacion1) || isNaN(calificacion2) ){
+	alert ("No es númerico ");
+} else {
+	var suma = parseInt(calificacion1) + parseInt(calificaion2);
+	alert(suma);
+	alert ("Es númerico");
+	
+}
---
+++ Josué Antonio Castro Collí/Programa 3.js
@@ -0,0 +1,15 @@
+/*
+****************************************
+* Source: Programa1.js 
+* Description: Funciones iniciales en js
+* Encender un motor / Apagar un motor 
+* Date: 7/Fec/2020
+* Author: Josue Antonio Castro Colli
+****************************************
+*/
+
+var calificacion=[];
+for (i=0; i< 10 ; i++){
+	calificacion [i] = prompt("Escribe una calificación númerica");
+}
+alert (calificacion);
