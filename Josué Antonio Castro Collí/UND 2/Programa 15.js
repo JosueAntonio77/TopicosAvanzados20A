12ec61bbf5de71638126fc2c81418f2919d6fe00
JosueAnthonio77 <60789948+JosueAntonio77@users.noreply.github.com>
2020-06-14 14:23:00 -0500

    Acrualizado

--- Josué Antonio Castro Collí/UND 2/Programa 15.js
+++ Josué Antonio Castro Collí/UND 2/Programa 15.js
@@ -1,18 +1,30 @@
 /*****************************************************************************
 # Program Name: Program 15.js
 # Authors: Josue Antonio Castro Collí. 
-# Description: Enviar mensajes con html al WhatsApp 
+# Description: Enviar un mensajes de WhatsApp con JavaScript. 
 # Date: 06/Marzo/2020
 #
 #Funciones:
-# crear ( )               //G
+# CrearMensaje ( )               // Genera el mensaje modificando la dirección web del WhatsApp
+#									con los digitos que el usuario introdujo.
 ******************************************************************************
 *****************************************************************************/
-function enviar() {
-	var mensaje = documet.getElementById('inputMensaje');
-	var numero = documet.getElementById('inputNum');
-	Enviado =  "https://api.whatsapp.com/send?phone=52"+numero+"&text="+mensaje;
+function CrearMensaje() {
+	//Obtener el numero Telefonico.
+	var numero = document.getElementById("inputNum").value;
+	//Obtener el mensaje escrito.
+	var mensaje = document.getElementById('message').value;
 
-	alert("Mensaje Enviado");
+	//Verificar si tiene llenado los dos campos que se solicita.
+	if (numero && mensaje){
+		//Para mandar al usuario a la página de WhatsApp y prosiga para enviar el mansaje.
+		location.href="https://api.whatsapp.com/send?phone="+numero+"&text="+mensaje;
+		//Montrar que todo sigue bien.
+		alert('Procesando');
+	} else {
+		//Pedirle que llene todos los campos si no lo hizo.
+		alert('¡No se relleno uno de los campos solicitados!');
+	}
 
+//https://api.whatsapp.com/send?phone=529993815121&text=Prueba 
 }
